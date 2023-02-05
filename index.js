4ee8c40f6ce2b7262e3e953c55c26bc4117f7933
mfflynnnn <89746780+mfflynnnn@users.noreply.github.com>
2023-02-05 02:00:45 -0800

    Refactored (see description)
    Refactored index.js to utilize Express instead of pure Node.js. Added preliminary movie data.

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+
+let topTenMovies = [
+  { title: "Inside Out", director: "Pete Docter", genre: "family animated" },
+  {
+    title: "Eternal Sunshine of the Spotless Mind",
+    director: "Michel Gondry",
+    genre: "drama",
+  },
+  {
+    title: "Brokeback Mountain",
+    director: "Ang Lee",
+    genre: "neo-Western romantic drama",
+  },
+  {
+    title: "My Beautiful Broken Brain",
+    director: "Sophie Robinson",
+    genre: "documentary",
+  },
+  {
+    title: "Star Trek: Voyager",
+    director: "Rick Berman",
+    genre: "science fiction",
+  },
+  {
+    title: "Everything Everywhere All at Once",
+    director: "Daniel Kwan",
+    genre: "comedy-drama",
+  },
+  {
+    title: "Sense and Sensibility",
+    director: "Ang Lee",
+    genre: "period drama",
+  },
+  {
+    title: "Moonlight",
+    director: "Barry Jenkins",
+    genre: "coming-of-age drama",
+  },
+  {
+    title: "Where the Lilies Bloom",
+    director: "William A Graham",
+    genre: "drama",
+  },
+  {
+    title: "The Sound of Music",
+    director: "Robert Wise",
+    genre: "musical drama",
+  },
+];
+
+let myLogger = (req, res, next) => {
+  console.log(req.url);
+};
+
+app.use(myLogger);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to myFlix!");
+});
+
+app.get("/movies", (req, res) => {
+  res.json(topTenMovies);
+});
+
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
